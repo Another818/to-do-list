0bdf82c3c1febd8a5933fccb1d80af1b31cb541b
fedeminatta <minattafederico@gmail.com>
2023-11-21 17:12:27 -0300

    eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,38 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'standard',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'eslint-config-prettier',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'no-unused-vars': 'warn',
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
