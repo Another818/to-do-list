9099b5c3a315ab6ecd0dda45c56863047ef93972
Tomás Menna <60326901+Another818@users.noreply.github.com>
2023-11-26 22:00:05 -0300

    Delete react-tasklist-project-final-version directory

--- react-tasklist-project-final-version/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import TaskForm from "./TaskForm/TaskForm";
-import TaskItem from "./TaskItem/TaskItem";
-import TaskList from "./TaskList/TaskList";
-
-export {
-    TaskForm,
-    TaskItem,
-    TaskList
-}
